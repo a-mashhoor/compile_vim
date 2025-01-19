7d16efd5a760c307517d84fb4761e120e64a06f1
arshia mashhoor <arshiamashhoor@gmail.com>
2025-01-19 10:43:23 +0330

    some minor changes

--- compile_vim.sh
+++ compile_vim.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+# Exit on Any error
+set -e
+
 # checking for root privileges
 if [[ $EUID -ne 0 ]]; then
 	echo "You must be root to run the script" 1>&2
@@ -8,27 +11,29 @@ fi
 
 # finding the right package manager
 
+# finding OS
 OS=$(uname -s | tr A-Z a-z)
 
+# finding distro and based on distro selecting a package manager
 case $OS in
 	linux)
 		echo -n OS is LINUX!
 		source /etc/os-release
 		case $ID_LIKE in
 			debian)
-				echo OS is Debian based
+				echo OS is Debian based selecting apt as package manager
 				purge_cmd="apt purge"
 				update_CMD="apt update"
 				install_CMD="apt install"
 				;;
 			arch*)
-				echo OS is Arch based
+				echo OS is Arch based selecting pacman as package manager
 				purge_cmd="pacman -Rs"
 				update_CMD="pacman -Syyu"
 				install_CMD="pacman -S"
 				;;
 			rhel*)
-				echo OS is Red Hat based
+				echo OS is Red Hat based selecting yum as package manager
 				purge_cmd="yum remove"
 				update_CMD="yum update"
 				install_CMD="yum install"
@@ -39,7 +44,7 @@ case $OS in
 		esac
 		;;
 	darwin)
-		echo OS is OS X using brew as package manager
+		echo OS is OS X selecting brew as package manager
 		purge_cmd="brew uninstall"
 		update_CMD="brew update"
 		install_CMD="brew install"
@@ -49,47 +54,77 @@ case $OS in
 		;;
 esac
 
-# update the repos
+# update the repositories
+echo Updating the repositories
 $update_CMD
 
-
-if ! git; then
+# Install git if not installed
+echo "Checking for Git installation..."
+if ! command -v git &>/dev/null; then
+	echo "Git not found, installing it"
 	$install_CMD git -y
 fi
 
-if ! python3; then
+# Install Python3 if not installed
+echo "Checking for Python3 installation..."
+if ! command -v python3 &>/dev/null; then
+	echo "Python3 not found, installing it"
 	$install_CMD python3-full -y
 fi
 
-if vim; then
+echo removing existing installation of vim if any
+if command -v vim &>/dev/null; then
+	echo vim installation found removing it
 	$purge_cmd vim vim-gtk3 vim-runtime gvim vim-tiny vim-common vim-gui-common -y
 fi
 
+echo Cloning vim in a while loop until the clone is finished
 while ! -d vim; do
 	git clone https://github.com/vim/vim.git vim
 done
 
+echo change directory to vim
 cd vim
 
+echo Installing dependencies
+dependencies=(
+	"libncurses5-dev"
+	"libgtk2.0-dev"
+	"libatk1.0-dev"
+	"libcairo2-dev"
+	"libx11-dev"
+	"libxpm-dev"
+	"libxt-dev"
+	"python3-dev"
+	"ruby-dev"
+	"mercurial"
+	"sed"
+	"awk"
+	"curl"
+	"make"
+	"nproc"
+)
 # Installing dependencies
-$install_CMD libncurses5-dev libgtk2.0-dev libatk1.0-dev libcairo2-dev libx11-dev \
-	libxpm-dev libxt-dev python3-dev ruby-dev mercurial sed awk curl -y
+$install_CMD "${dependencies[@]}" -y
 
-python3version=$(python3 --version | awk '{print $2}' | sed -e 's/..$//g')
+python_v=$(python3 --version | awk '{print $2}' | sed -e 's/..$//g')
+python_config_dir="/usr/lib/python$python_v/config-$python_v-x86_64-linux-gnu/"
 
-# my custom config for compiling
+# my custom config for compiling vim
+echo configuring VIM
 ./configure --with-features=huge --enable-multibyte --with-x --enable-rubyinterp \
-	--enable-python3interp --with-python-config-dir=/usr/lib/python$python3version/config-$python3version-x86_64-linux-gnu/ \
+	--enable-python3interp --with-python-config-dir=$python_config_dir \
 	--with-python3-command=python3 --enable-terminal --enable-cscope \
-	--prefix=/usr/local --enable-gtk2-check --enable-farsi --enable-gtk3-check \
+	--prefix=/usr/local --enable-gtk2-check --enable-gtk3-check \
 	--enable-gtktest --enable-gui=auto
 
-# making and building binary !!!!
-make VMRUNTIMEDIR=/usr/share/vim/vim9
-make install
+# building and installing vim
+echo Building and installing VIM
+sudo make -j$(nproc) VMRUNTIMEDIR=/usr/share/vim/vim9
+sudo make install
 
 # Creating vim related directories for user
-mkdir -p ~/.vim/{autoload,backup,colors,plugged}
+mkdir -p ~/.vim/{backup,colors,plugged}
 
 # vim plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
